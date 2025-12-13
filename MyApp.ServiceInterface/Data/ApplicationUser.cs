285b302ebaf7dd794e749a6602ad2e36fe6a4e4c
Demis Bellot <demis.bellot@gmail.com>
2025-12-14 01:19:29 +0800

    Update ApplicationUser.cs

--- MyApp.ServiceInterface/Data/ApplicationUser.cs
+++ MyApp.ServiceInterface/Data/ApplicationUser.cs
@@ -2,6 +2,11 @@ using Microsoft.AspNetCore.Identity;
 
 namespace MyApp.Data;
 
+/* After modifying ApplicationUser, delete and recreate the initial EF Core migration with:
+   rm Migrations/202*.cs Migrations/ApplicationDbContextModelSnapshot.cs
+   dotnet ef migrations add CreateIdentitySchema
+*/
+
 // Add profile data for application users by adding properties to the ApplicationUser class
 public class ApplicationUser : IdentityUser
 {
